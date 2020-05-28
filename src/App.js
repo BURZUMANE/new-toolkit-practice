29a867086d332bd81252f9d833ad92cb924e347e
Timur Zakirov <1234tima.g@gmail.com>
2020-05-28 19:00:54 +0300

    beta

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { newsSlice } from './redux/store';
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.css';
+import NewsList from './components/newsList/NewsList';
+import SingleNewsItem from './components/singleNewsItem/SingleNewsItem';
+const URL =
+  'http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-28&sortBy=publishedAt&apiKey=baf3b32ef95d4cd0803dcc5d7a5bf9b1';
+
+function App() {
+  const la = '';
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getNews = async () => {
+      const data = await axios.get(URL);
+      dispatch(
+        newsSlice.actions.addNews(
+          data.data.articles.map((item) => ({ ...item, id: uuidv4() }))
+        )
+      );
+    };
+    getNews();
+    // axios.get(URL).then((data) => {
+    //   dispatch(newsSlice.actions.addNews(data.data.articles));
+    // });
+  }, []); // Only re-run the effect if count changes
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={NewsList}></Route>
+
+        <Route path="/news/:id" component={SingleNewsItem}></Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
